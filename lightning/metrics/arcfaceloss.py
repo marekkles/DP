99391341b13e0ef31ba5a83ed3e5c30fb98fd5d1
Marek Vasko <xvasko16@vutbr.cz>
2022-03-10 09:01:43 +0100

    Added batch norm

--- lightning/metrics/arcfaceloss.py
+++ lightning/metrics/arcfaceloss.py
@@ -10,12 +10,16 @@ class ArcFaceLoss(nn.Module):
         super(ArcFaceLoss, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.bn = nn.BatchNorm1d(in_features, eps=2e-05, momentum=0.9)
         self.fc = ArcFace(s, m)
         self.weight = torch.nn.Parameter(torch.FloatTensor(out_features, in_features))
         self.loss = nn.CrossEntropyLoss()
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
         nn.init.xavier_uniform_(self.weight)
     def forward(self, input, label):
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        bn = self.bn(input)
+        cosine = F.linear(F.normalize(bn), F.normalize(self.weight))
         fc = self.fc(cosine, label)
         return self.loss(fc, label), cosine
     
@@ -24,12 +28,16 @@ class CosFaceLoss(nn.Module):
         super(ArcFaceLoss, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.bn = nn.BatchNorm1d(in_features, eps=2e-05, momentum=0.9)
         self.fc = CosFace(s, m)
         self.weight = torch.nn.Parameter(torch.FloatTensor(out_features, in_features))
         self.loss = nn.CrossEntropyLoss()
+        nn.init.constant_(self.bn.weight, 1)
+        nn.init.constant_(self.bn.bias, 0)
         nn.init.xavier_uniform_(self.weight)
     def forward(self, input, label):
-        cosine = F.linear(F.normalize(input), F.normalize(self.weight))
+        bn = self.bn(input)
+        cosine = F.linear(F.normalize(bn), F.normalize(self.weight))
         fc = self.fc(cosine, label)
         return self.loss(fc, label), cosine
 
